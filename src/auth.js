83a5046a5a7ab6123a26511ba0e6a5c7c1aa022d
Saurabh Batra <saurabh.batra96@gmail.com>
2016-06-02 22:46:12 +0530

    get download working

--- src/auth.js
+++ src/auth.js
@@ -12,7 +12,7 @@ var deasync = require('deasync');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/drive-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/drive'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'drive-sync-auth.json';
